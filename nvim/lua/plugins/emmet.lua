94d8444507ca1da4265028bd5c4917a8aa248c1f
mogurina <mogumogutyannneru@icloud.com>
2024-12-26 14:38:40 +0900

    add emmet settings

--- nvim/lua/plugins/emmet.lua
+++ nvim/lua/plugins/emmet.lua
@@ -1,6 +1,25 @@
-return {
-	'mattn/emmet-vim',
-	config = function()
-		vim.g.user_emmet_leader_key = '<C-y>'
-	end
+--vim.g['emmet_html5'] = false
+vim.g['user_emmet_install_global'] = false
+
+vim.g['user_emmet_settings'] = {
+  variables = {
+    lang = 'ja'
+  }
 }
+--vim.g['user_emmet_leader_key'] = '<tab>'
+vim.g['user_emmet_expandabbr_key'] = '<tab>'
+
+local config = function()
+	vim.api.nvim_create_autocmd('FileType', {
+		pattern = { 'vue', 'html', 'css', 'typescriptreact' },
+		command = 'EmmetInstall',
+		group = vim.api.nvim_create_augroup("emmetInstall", { clear = true }),
+	})
+end
+
+local emmet = {
+	'mattn/emmet-vim',
+	config = config
+}
+
+return emmet
