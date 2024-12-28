d685fa43cb0f1438a4c235fd76b2c1c1a67e0410
mogurina <mogumogutyannneru@icloud.com>
2024-12-28 19:15:06 +0900

    change emmet settings

--- nvim/lua/plugins/emmet.lua
+++ nvim/lua/plugins/emmet.lua
@@ -11,7 +11,7 @@ vim.g['user_emmet_expandabbr_key'] = '<tab>'
 
 local config = function()
 	vim.api.nvim_create_autocmd('FileType', {
-		pattern = { 'vue', 'html', 'css', 'typescriptreact' },
+		pattern = { 'vue', 'html', 'css', 'typescriptreact',"typescript","javascript","javascriptreact"},
 		command = 'EmmetInstall',
 		group = vim.api.nvim_create_augroup("emmetInstall", { clear = true }),
 	})
