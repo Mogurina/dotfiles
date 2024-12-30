f465a55bb3216f33ac55ae175f7e1d216b434663
mogurina <mogumogutyannneru@icloud.com>
2024-12-31 08:15:31 +0900

    remove keymap

--- nvim/lua/keymap.lua
+++ nvim/lua/keymap.lua
@@ -2,13 +2,3 @@
 local keymap = vim.keymap
 keymap.set('i','jk','<ESC>')
 keymap.set('v','<leader>jk','<ESC>')
---keymap.set('t','<leader>jk','<C-\\><C-n>')
-
---keymap.set('n','tt','<cmd>belowright new<CR><cmd>terminal<CR>',{silent=true})
---keymap.set('n','tx','<cmd>terminal<CR>',{silent=true})
---vim.cmd('autocmd TermOpen * :startinsert')
---vim.cmd('autocmd TermOpen * setlocal norelativenumber')
---vim.cmd('autocmd TermOpen * setlocal nonumber')
-
-
-
