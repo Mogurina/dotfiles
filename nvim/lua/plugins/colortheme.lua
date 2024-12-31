24d922fadd1dc01cf38b963741caea2375f4a373
mogurina <mogumogutyannneru@icloud.com>
2025-01-01 03:32:57 +0900

    add seings

--- nvim/lua/plugins/colortheme.lua
+++ nvim/lua/plugins/colortheme.lua
@@ -21,6 +21,36 @@ return {
 				hide_inactive_statusline = false,
 				dim_inactive = false,
 				lualine_bold = true,
+				on_highlights = function(hl, c)
+					local prompt = "#2d3149"
+					hl.TelescopeNormal = {
+						bg = c.bg_dark,
+						fg = c.fg_dark,
+					}
+					hl.TelescopeBorder = {
+						bg = c.bg_dark,
+						fg = c.bg_dark,
+					}
+					hl.TelescopePromptNormal = {
+						bg = c.bg_dark,
+					}
+					hl.TelescopePromptBorder = {
+						bg = c.bg_dark,
+						fg = c.bg_dark,
+					}
+					hl.TelescopePromptTitle = {
+						bg = c.bg_dark,
+						fg = c.bg_dark,
+					}
+					hl.TelescopePreviewTitle = {
+						bg = c.bg_dark,
+						fg = c.bg_dark,
+					}
+					hl.TelescopeResultsTitle = {
+						bg = c.bg_dark,
+						fg = c.bg_dark,
+					}
+				end
 			})
 			vim.cmd("highlight Normal guibg=None,ctermbg=None")
 		end,
