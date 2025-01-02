3214d79ee300a9d835445179a6ae0eafad0ff500
mogurina <mogumogutyannneru@icloud.com>
2025-01-03 07:14:47 +0900

    add tailwind warning ignore

--- nvim/lua/plugins/autocmp.lua
+++ nvim/lua/plugins/autocmp.lua
@@ -7,7 +7,7 @@ return {
 		},
 		config = function()
 			require("mason").setup()
-			
+
 			local registry = require("mason-registry")
 			local packages = {
 				"gopls",
@@ -32,68 +32,77 @@ return {
 					local opt = {
 						-- -- Function executed when the LSP server startup
 						-- on_attach = function(client, bufnr)
-						--   local opts = { noremap=true, silent=true }
-						--   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-						--   vim.cmd 'autocmd BufWritePre * lua vim.lsp.buf.formatting_sync(nil, 1000)'
-						-- end,
-						capabilities = require('cmp_nvim_lsp').default_capabilities(
-						  vim.lsp.protocol.make_client_capabilities()
-						)
-					}
-					require("lspconfig")[server].setup(opt)
-				end,
-			})
-		end
-	},
-	{
-		"hrsh7th/nvim-cmp",
-		event = "InsertEnter",
-		dependencies = {
-			"hrsh7th/cmp-buffer",
-			"hrsh7th/cmp-path",
-			"hrsh7th/cmp-nvim-lsp",
-			"hrsh7th/vim-vsnip",
+							--   local opts = { noremap=true, silent=true }
+							--   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+							--   vim.cmd 'autocmd BufWritePre * lua vim.lsp.buf.formatting_sync(nil, 1000)'
+							-- end,
+							capabilities = require('cmp_nvim_lsp').default_capabilities(
+							vim.lsp.protocol.make_client_capabilities()
+							)
+						}
+						require("lspconfig")[server].setup(opt)
+					end,
+				})
+				require('lspconfig').cssls.setup({
+					settings = {
+						css = {
+							lint = {
+								unknownAtRules = 'ignore',
+							},
+						},
+					},
+				})
+			end,
 		},
-		config = function()
-			local cmp = require("cmp")
+		{
+			"hrsh7th/nvim-cmp",
+			event = "InsertEnter",
+			dependencies = {
+				"hrsh7th/cmp-buffer",
+				"hrsh7th/cmp-path",
+				"hrsh7th/cmp-nvim-lsp",
+				"hrsh7th/vim-vsnip",
+			},
+			config = function()
+				local cmp = require("cmp")
 
-			cmp.setup({
-				window = {
-					completion = cmp.config.window.bordered({winhighlight="Normal:PopMenu,FloatBorder:None,CursorLine:Visual,Search:None"}),
-					documentation = cmp.config.window.bordered({winhighlight="Normal:PopMenu,FloatBorder:None,CursorLine:Visual,Search:None"}),
-				},
-				completion = {
-					completion = "menu,menuone,noinsert",
-				},
-				snippet = {
-					expand = function(args)
-						vim.fn["vsnip#anonymous"](args.body)
+				cmp.setup({
+					window = {
+						completion = cmp.config.window.bordered({winhighlight="Normal:PopMenu,FloatBorder:None,CursorLine:Visual,Search:None"}),
+						documentation = cmp.config.window.bordered({winhighlight="Normal:PopMenu,FloatBorder:None,CursorLine:Visual,Search:None"}),
+					},
+					completion = {
+						completion = "menu,menuone,noinsert",
+					},
+					snippet = {
+						expand = function(args)
+							vim.fn["vsnip#anonymous"](args.body)
+						end,
+					},
+					sources = {
+						{ name = "nvim_lsp" },
+						{ name = "buffer" },
+						{ name = "path" },
+						{ name = "render-markdown" },
+					},
+					mapping = cmp.mapping.preset.insert({
+						["<S-TAB>"] = cmp.mapping.select_prev_item(),
+						["<TAB>"] = cmp.mapping.select_next_item({behavior = cmp.SelectBehavior.Insert}),
+						['<C-l>'] = cmp.mapping.complete(),
+						['<C-e>'] = cmp.mapping.abort(),
+						["<CR>"] = cmp.mapping.confirm { select = true },
+					}),
+					experimental = {
+						ghost_text = true,
+					},
+				})
+				--cmp.setup.cmdline(":", {
+					--	completion = {
+						--		completeopt = "menu,menuone,noinsert,noselect",
+						--	},
+						--})
 					end,
 				},
-				sources = {
-					{ name = "nvim_lsp" },
-					{ name = "buffer" },
-					{ name = "path" },
-					{ name = "render-markdown" },
-				},
-				mapping = cmp.mapping.preset.insert({
-					["<S-TAB>"] = cmp.mapping.select_prev_item(),
-					["<TAB>"] = cmp.mapping.select_next_item({behavior = cmp.SelectBehavior.Insert}),
-					['<C-l>'] = cmp.mapping.complete(),
-					['<C-e>'] = cmp.mapping.abort(),
-					["<CR>"] = cmp.mapping.confirm { select = true },
-				}),
-				experimental = {
-					ghost_text = true,
-				},
-			})
-			--cmp.setup.cmdline(":", {
-			--	completion = {
-			--		completeopt = "menu,menuone,noinsert,noselect",
-			--	},
-			--})
-		end,
-	},
-}
+			}
 
 
