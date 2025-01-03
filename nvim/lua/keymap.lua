27c5229bc2dc12cef610a26a9f2305b9dc14e144
mogurina <mogumogutyannneru@icloud.com>
2025-01-04 00:05:41 +0900

    change v esc keymap

--- nvim/lua/keymap.lua
+++ nvim/lua/keymap.lua
@@ -1,6 +1,6 @@
 --keymap
 local keymap = vim.keymap
 keymap.set('i','jk','<ESC>')
-keymap.set('v','<leader>jk','<ESC>')
+keymap.set('v','jk','<ESC>')
 keymap.set("n","F","<C-f>")
 keymap.set("n","B","<C-b>")
