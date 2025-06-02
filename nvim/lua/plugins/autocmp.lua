cd826e206c9920e265de93018c3c00b24178c49b
mogurina <mogumogutyannneru@icloud.com>
2025-06-02 20:53:58 +0900

    update autocmp

--- nvim/lua/plugins/autocmp.lua
+++ nvim/lua/plugins/autocmp.lua
@@ -36,6 +36,11 @@ return {
 							--   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
 							--   vim.cmd 'autocmd BufWritePre * lua vim.lsp.buf.formatting_sync(nil, 1000)'
 							-- end,
+							init_options = {
+								preferences = {
+										disableSuggestions = true
+								}
+							},
 							capabilities = require('cmp_nvim_lsp').default_capabilities(
 							vim.lsp.protocol.make_client_capabilities()
 							)
