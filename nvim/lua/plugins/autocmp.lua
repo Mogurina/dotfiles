e61508ce83e34d8922fa85b95a4c5194f4cf1e89
mogurina <mogumogutyannneru@icloud.com>
2025-06-27 00:01:51 +0900

    update mason settings

--- nvim/lua/plugins/autocmp.lua
+++ nvim/lua/plugins/autocmp.lua
@@ -2,7 +2,7 @@ return {
 	{
 		"williamboman/mason-lspconfig.nvim",
 		dependencies = {
-			{ "williamboman/mason.nvim" },
+			{ "williamboman/mason.nvim"},
 			{ "neovim/nvim-lspconfig" },
 		},
 		config = function()
@@ -27,27 +27,6 @@ return {
 			end)
 
 			require("mason-lspconfig").setup()
-			require("mason-lspconfig").setup_handlers({
-				function(server)
-					local opt = {
-						-- -- Function executed when the LSP server startup
-						-- on_attach = function(client, bufnr)
-							--   local opts = { noremap=true, silent=true }
-							--   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-							--   vim.cmd 'autocmd BufWritePre * lua vim.lsp.buf.formatting_sync(nil, 1000)'
-							-- end,
-							init_options = {
-								preferences = {
-										disableSuggestions = true -- Disable inline suggestions
-								}
-							},
-							capabilities = require('cmp_nvim_lsp').default_capabilities(
-							vim.lsp.protocol.make_client_capabilities()
-							)
-						}
-						require("lspconfig")[server].setup(opt)
-					end,
-				})
 				local capabilities = vim.lsp.protocol.make_client_capabilities()
 				capabilities.textDocument.completion.completionItem.snippetSupport = true
 				require('lspconfig').cssls.setup({
