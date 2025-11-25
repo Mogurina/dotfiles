2e6d554ddfcce120f3c42afd89f402963217866b
Mogurina <mogumogutyannneru@icloud.com>
2025-11-26 01:49:12 +0900

    update

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -19,11 +19,7 @@ local neovim_plugins = {
 	require("plugins.lualine"),
 	require("plugins.nvim-autopairs"), --文字の挿入を伴うプラグインは'InsertEnter'を指定する。
 	require("plugins.fidget"), --LSPと連動するプラグインは'LspAttach'を指定する。
-	--require("plugins.telescope"),--telescope	
-	--require("plugins.telescope-fzf"),--telescope snacks pickerに置き換えた
-	--require("plugins.plenary"),--telescope
 	require("plugins.treesitter"),--シンタックスハイライト関係
-	--require("plugins.emmet"),
 	require("plugins.highlight-colors"),
 	require("plugins.nvim-notify"),
 	require("plugins.noice"),
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,6 @@
 return {
 	"nvim-treesitter/nvim-treesitter", 
+	event = "BufReadPost",
 	cond=true,
 	build = ":TSUpdate",
 	config = function()
