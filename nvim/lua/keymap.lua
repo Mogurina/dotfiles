05604583a90ddb63c6610850a60fe6a78b0735a1
mogurina <mogumogutyannneru@icloud.com>
2025-12-14 09:38:50 +0900

    add bufferline keymap

--- nvim/lua/keymap.lua
+++ nvim/lua/keymap.lua
@@ -1,27 +1,28 @@
-if not vim.g.vscode then
-	-- Neovimのキーマップ設定
-	--keymap
-	local keymap = vim.keymap
-	keymap.set('i','jk','<ESC>')
-	keymap.set('v','jk','<ESC>')
-	keymap.set("n","F","<C-f>")
-	keymap.set("n","B","<C-b>")
+-- Neovimのキーマップ設定
+--keymap
+local keymap = vim.keymap
+keymap.set('i','jk','<ESC>')
+keymap.set('v','jk','<ESC>')
+keymap.set("n","F","<C-f>")
+keymap.set("n","B","<C-b>")
 
+--bufferline
+keymap.set("n","<leader>h","<cmd>bnext<CR>")
+keymap.set("n","<leader>l","<cmd>bprev<CR>")
 
-	keymap.set("n","<Leader>c",":CopilotChatToggle<Return>")
+keymap.set("n","<Leader>c",":CopilotChatToggle<Return>")
 
-	-- 画面分割
-	keymap.set('n', '<Leader>sv', ':split<Return><C-w>w')
-	keymap.set('n', '<Leader>s', ':vsplit<Return><C-w>w')
-	-- アクティブウィンドウの移動
-	keymap.set('n', '<Leader>sh', '<C-w>h')
-	keymap.set('n', '<Leader>sk', '<C-w>k')
-	keymap.set('n', '<Leader>sj', '<C-w>j')
-	keymap.set('n', '<Leader>sl', '<C-w>l')
+-- 画面分割
+keymap.set('n', '<Leader>sv', ':split<Return><C-w>w')
+keymap.set('n', '<Leader>s', ':vsplit<Return><C-w>w')
+-- アクティブウィンドウの移動
+keymap.set('n', '<Leader>sh', '<C-w>h')
+keymap.set('n', '<Leader>sk', '<C-w>k')
+keymap.set('n', '<Leader>sj', '<C-w>j')
+keymap.set('n', '<Leader>sl', '<C-w>l')
 
-	--　インデントで折り畳み
-	keymap.set("n", "<Tab>", "zc")
-	keymap.set("n", "<S-Tab>", "zo")
-	keymap.set("n", "<Leader><Tab>", "zM")
-	keymap.set("n", "<Leader><S-Tab>", "zR")
-end
+--　インデントで折り畳み
+keymap.set("n", "<Tab>", "zc")
+keymap.set("n", "<S-Tab>", "zo")
+keymap.set("n", "<Leader><Tab>", "zM")
+keymap.set("n", "<Leader><S-Tab>", "zR")
