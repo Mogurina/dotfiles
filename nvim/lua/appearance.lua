92b9e7e2c152947336e8408831223d18b1616ff3
Mogurina <mogumogutyannneru@icloud.com>
2026-04-20 13:53:40 +0900

    update

--- nvim/lua/appearance.lua
+++ nvim/lua/appearance.lua
@@ -1,9 +1,7 @@
 --colo0
 --vim.cmd[[colorscheme tokyonight-moon]]
 vim.cmd[[colorscheme tokyonight-night]]
---vim.cmd[[colorscheme everforest]]
---vim.cmd[[colorscheme catppuccin-mocha]]
+
 
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
-
